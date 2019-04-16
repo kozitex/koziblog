1a944e3f08e3e9fb10211291509e808b9b4a4a7a
kozitex <grassrunners@gmail.com>
2019-04-16 16:59:38 +0900

    Preparing heroku deproy 06

--- db/migrate/20190201155402_change_datatype_user_id_of_posts.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeUserIdOfPosts < ActiveRecord::Migration[5.2]
-  def change
-    change_column :posts, :user_id, :integer
-  end
-end
--- db/migrate/20190416073751_change_data_user_id_to_post.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataUserIdToPost < ActiveRecord::Migration[5.2]
-  def change
-    change_column :posts, :user_id, :integer USING user_id::integer
-  end
-end
