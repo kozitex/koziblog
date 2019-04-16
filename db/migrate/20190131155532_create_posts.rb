3a9c064be4ac01a3e3ba39d7be69e1fb3164f798
kozitex <grassrunners@gmail.com>
2019-04-16 16:21:46 +0900

    Preparing heroku deproy 02

--- db/migrate/20190131155532_create_posts.rb
+++ db/migrate/20190131155532_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :content, null: false
-      t.string :user_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
