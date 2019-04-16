094ce4cd8de3affe2b9046bae9fa155572148221
kozitex <grassrunners@gmail.com>
2019-04-16 16:52:29 +0900

    Preparing heroku deproy 05

--- db/migrate/20190416073751_change_data_user_id_to_post.rb
+++ db/migrate/20190416073751_change_data_user_id_to_post.rb
@@ -1,5 +1,5 @@
 class ChangeDataUserIdToPost < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :user_id, 'integer USING CAST(column_name AS integer)'
+    change_column :posts, :user_id, :integer USING user_id::integer
   end
 end
